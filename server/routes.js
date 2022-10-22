622102497261267d54c00cc6b23bbdc6a5509215
JonatSrG <JonatSrG>
2022-10-22 12:34:22 -0500

    Api Rest - Post Mensaje

--- server/routes.js
+++ server/routes.js
@@ -35,6 +35,22 @@ router.get('/', function (req, res) {
   res.json( mensajes );
 });
 
+router.post('/', function (req, res) {
+  
+  const mensaje = {
+    mensaje: req.body.mensaje,
+    user: req.body.user
+  };
+
+  mensajes.push( mensaje );
+
+  res.json( {
+    ok: true,
+    mensaje
+  } );
+
+});
+
 
 
 
